458b937ec28564e7ef38a9335ee07e39406b114b
Pauliina Solanne <pauliinasolanne@Pauliina-MacBook-Air.local>
2016-06-03 10:49:16 +1000

    delete

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,4 +5,10 @@ class AdminController < ApplicationController
     @users = User.all
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to '/userlist'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
 
   delete '/profile' => 'users#destroy'
 
+  delete '/userlist/:id' => 'admin#destroy'
+
 
   resources :users
 
